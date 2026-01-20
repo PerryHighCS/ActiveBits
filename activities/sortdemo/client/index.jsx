8e79a3d3e963ad3c2734dd6bce1ac0a4d9ccb758
Brian Dahlem <dahlem.brian@cusd80.com>
2026-01-20 22:12:18 +0000

    removed sortdemo and guessing game exemplars

--- activities/sortdemo/client/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-// Legacy sortdemo - just a placeholder since activity.config.js marks as isDev
-export default {
-  ManagerComponent: () => <div>Sort Demo (Legacy - use Algorithm Demo instead)</div>,
-  StudentComponent: () => <div>Sort Demo (Legacy - use Algorithm Demo instead)</div>,
-  footerContent: null,
-};
