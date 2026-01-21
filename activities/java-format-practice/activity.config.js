845ed90adaaaac04ecf67d246193a831cd144c62
Brian Dahlem <dahlem.brian@cusd80.com>
2026-01-21 01:32:20 +0000

    set color for java format practice cards

--- activities/java-format-practice/activity.config.js
+++ activities/java-format-practice/activity.config.js
@@ -2,7 +2,7 @@ export default {
   id: 'java-format-practice',
   name: 'Java Format Practice',
   description: 'Interactive practice for Java printf and String.format',
-  color: 'orange',
+  color: 'emerald',
   soloMode: true,
   clientEntry: './client/index.js',
   serverEntry: './server/routes.js',
